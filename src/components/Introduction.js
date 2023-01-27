273b2e55c3fc479ea119b8581bd791eb9efbf18f
Darryll Robinson <darryll@stillproud.com>
2023-01-27 14:45:25 +1000

    Blog style example

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,32 +1,55 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Box, Grid, Link, Paper, Typography } from '@mui/material';
 
 import helen from '../assets/images/helen.jpeg';
 
 export default function Introduction() {
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image={helen}
-                    alt="Helen Digby"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Welcome to Joint Prosperity
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <Paper
+            sx={{
+                position: 'relative',
+                backgroundColor: 'grey.800',
+                color: '#fff',
+                mb: 4,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundImage: { helen },
+            }}
+        >
+            {/* Increase the priority of the hero background image */}
+            {<img style={{ display: 'none' }} src={helen} alt="Helen Digby" />}
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    backgroundColor: 'rgba(0,0,0,.3)',
+                }}
+            />
+            <Grid container>
+                <Grid item md={6}>
+                    <Box
+                        sx={{
+                            position: 'relative',
+                            p: { xs: 3, md: 6 },
+                            pr: { md: 0 },
+                        }}
+                    >
+                        <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                            Joint Prosperity
+                        </Typography>
+                        <Typography variant="h5" color="inherit" paragraph>
+                            Welcome to JP
+                        </Typography>
+                        <Link variant="subtitle1" href="#">
+                            Links
+                        </Link>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Paper>
     );
 }
