08128f252510cf0e1a91bf0c95aad274ef815a24
Darryll Robinson <darryll@stillproud.com>
2023-01-27 15:00:52 +1000

    Moved components from App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import { Container, CssBaseline } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import ResponsiveDrawer from './components/ResponsiveDrawer';
+import Main from './components/Main';
 
 const theme = createTheme();
 
 function App() {
-  return (<ThemeProvider theme={theme}>
-    <CssBaseline />
-    <Container maxWidth="lg">
-      <ResponsiveDrawer />
-    </Container>
-  </ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Main />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material';
+
+import ResponsiveDrawer from './ResponsiveDrawer';
+
+export default function Main() {
+    return (
+        {/* <Container maxWidth="lg"> */ }
+        < ResponsiveDrawer />
+        {/* </Container> */ }
+    );
+}
